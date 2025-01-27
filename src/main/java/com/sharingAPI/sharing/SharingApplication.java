cb9975709488b76ccde6567c19536b8662a24074
rimapaulione <rimapaulione@gmail.com>
2025-01-27 21:32:51 +0200

    first commit

---
+++ src/main/java/com/sharingAPI/sharing/SharingApplication.java
@@ -0,0 +1,19 @@
+package com.sharingAPI.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SharingApplication {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+
+
+		SpringApplication.run(SharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharingAPI/sharing/SharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharingAPI.sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
